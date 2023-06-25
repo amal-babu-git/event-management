7bbb47270abed8fe4ab4f11850996a24bbc0a97e
Amal Babu <amalbabu1200@gmail.com>
2023-06-25 11:53:59 +0530

    auth base done, apps just created

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+# from django.db.models.query import QuerySet
+from . import models
+from typing import Sequence
+
+
+@admin.register(models.User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email', 'full_name', 'user_type', 'is_active', 'is_staff']
+
+    search_fields = ['full_name', 'email']
+    list_editable: Sequence[str] = [
+        'full_name', 'user_type', 'is_active', 'is_staff']
